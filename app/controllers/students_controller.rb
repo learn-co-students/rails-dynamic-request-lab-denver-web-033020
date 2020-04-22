aeb1757c935590c5af2382f986c8c87f92c911fc
jason-nordheim <jason.nordheim@gmail.com>
2020-04-22 12:46:38 -0400

    everything works, but my tests do not pass

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,12 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show 
+    if params[:id] != nil 
+      @student  = Student.find(params[:id])
+      @students = [@student]
+      render 'index'
+    end 
+  end 
 end
